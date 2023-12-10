d96b9899135ebff4bb7bf6e26ba0c04289d2cc8a
Sulaiman1015 <sulaiman1015@gmail.com>
2023-12-10 12:24:31 +0100

    modified files

--- terraform/create_config_ec2.tf
+++ terraform/create_config_ec2.tf
@@ -17,65 +17,87 @@ resource "tls_private_key" "mypem" {
 }
 
 resource "aws_key_pair" "myawskey" {
-  key_name   = "awskey"
+  key_name   = "myawskey"
   public_key = tls_private_key.mypem.public_key_openssh
 }
 
-resource "local_file" "mykeyfile" {
-  content          = tls_private_key.mypem.private_key_pem
-  filename         = "${path.module}/awskey.pem"
-  file_permission  = "0600"
+resource "local_file" "pem_file" {
+  filename             = pathexpand("~/.ssh/${local.ssh_key_name}.pem")
+  file_permission      = "0600"
+  directory_permission = "0700"
+  content              = tls_private_key.mypem.private_key_pem
+}
+
+locals {
+  ssh_key_name = "MyawsKey"
 }
 
 resource "aws_vpc" "sulaiman_vpc" {
-  cidr_block        = "192.168.0.0/16"
-  instance_tenancy  = "default"
+  cidr_block       = "10.0.0.0/16"
 
   tags = {
     Name = "sulaiman_vpc"
   }
 }
 
-resource "aws_subnet" "sulaiman_subnet" {
+resource "aws_subnet" "sulaiman_public_subnet" {
   vpc_id      = aws_vpc.sulaiman_vpc.id
-  cidr_block  = "192.168.1.0/24"
-  map_public_ip_on_launch = true
+  cidr_block  = "10.0.1.0/24"
+  availability_zone = "3a"
+
+  tags = {
+    Name = "sulaiman_pub_subnet"
+  }
+}
+
+resource "aws_subnet" "sulaiman_private_subnet" {
+  vpc_id      = aws_vpc.sulaiman_vpc.id
+  cidr_block  = "10.0.2.0/24"
+  availability_zone = "3a"
+
+  tags = {
+    Name = "sulaiman_private_subnet"
+  }
 }
 
 resource "aws_security_group" "sulaiman_sg" {
-  name        = "sulaiman_ssh"
+  name        = "sulaiman_ssh_http"
   vpc_id      = aws_vpc.sulaiman_vpc.id
-  description = "Allow SSH access"
+  description = "Allow SSH and HTTP access"
 
-  ingress = [{
-    description      = "ingress port 22 allow"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0",] # Your current ip, or change to 0.0.0.0/0 to allow all
-    self             = true
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    security_groups  = []
-  }]
+  # Rule allowing HTTP (port 80) access from anywhere for control node
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # You can restrict this to a specific IP range if needed
+    description = "HTTP from any IP for control node"
+  }
 
-  egress = [{
-    cidr_blocks      = ["0.0.0.0/0"]
-    description      = "egress allow all"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    self             = false
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    security_groups  = []
-  }]
+  # Rule allowing SSH (port 22) access from anywhere for all nodes
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # You can restrict this to a specific IP range if needed
+    description = "SSH from any IP for all nodes"
+  }
 
+  # Rule allowing all outbound traffic from all nodes
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
 
   tags = {
-    Name = "sulaiman_ssh_security_group"
+    Name = "sulaiman_sg"
   }
 }
+
+
 
 
 # Setup Internet gateway
@@ -88,7 +110,7 @@ resource "aws_internet_gateway" "sulaiman_igw" {
 }
 
 # Setup Internet router
-resource "aws_route_table" "sulaiman_route_table" {
+resource "aws_route_table" "sulaiman_public_rt" {
   vpc_id = aws_vpc.sulaiman_vpc.id
 
   route {
@@ -96,25 +118,39 @@ resource "aws_route_table" "sulaiman_route_table" {
     gateway_id = aws_internet_gateway.sulaiman_igw.id
   }
 
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.sulaiman_igw.id
+  }
+
   tags = {
-    Name = "sulaiman_route_table"
+    Name = "sulaiman_public_rt"
   }
 }
 
-resource "aws_route" "sulaiman_route" {
-  route_table_id            = aws_route_table.sulaiman_route_table.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                = aws_internet_gateway.sulaiman_igw.id
+resource "aws_route_table_association" "sulaiman_public_rta" {
+  subnet_id      = aws_subnet.sulaiman_public_subnet
+  route_table_id = aws_route_table.sulaiman_public_rt.id
 }
 
 resource "aws_instance" "control_node" {
   ami                             = "ami-0302f42a44bf53a45"
   instance_type                   = "t2.micro"
-  subnet_id                       = aws_subnet.sulaiman_subnet.id
-  security_groups                 = [aws_security_group.sulaiman_sg.id]
-  associate_public_ip_address     = true
   key_name                        = aws_key_pair.myawskey.key_name
 
+  subnet_id                       = aws_subnet.sulaiman_public_subnet.id
+  vpc_security_group_ids          = [aws_security_group.sulaiman_web_sg.id]
+  associate_public_ip_address     = true
+
+  user_data = <<-EOF
+  #!/bin/bash -ex
+
+  amazon-linux-extras install nginx1 -y
+  echo "<h1>The simplest thing is to learn. Congratulations, you succeeded!</h1>" >  /usr/share/nginx/html/index.html 
+  systemctl enable nginx
+  systemctl start nginx
+  EOF
+  
   tags = {
     Name = "sulaiman-control-node"
     Type = "control-node"
@@ -124,11 +160,11 @@ resource "aws_instance" "control_node" {
 resource "aws_instance" "managed_node" {
   ami                             = "ami-0302f42a44bf53a45"
   instance_type                   = "t2.micro"
-  subnet_id                       = aws_subnet.sulaiman_subnet.id
-  security_groups                 = [aws_security_group.sulaiman_sg.id]
-  associate_public_ip_address     = true
   key_name                        = aws_key_pair.myawskey.key_name
 
+  subnet_id                       = aws_subnet.sulaiman_private_subnet
+  vpc_security_group_ids          = [aws_security_group.sulaiman_private_sg]
+  associate_public_ip_address     = false
   count = 2
 
   tags = {
