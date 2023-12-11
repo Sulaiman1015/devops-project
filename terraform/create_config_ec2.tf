4cf2288d65c38ac793bea0c1d355f75ad76324fb
Sulaiman1015 <sulaiman1015@gmail.com>
2023-12-11 15:01:32 +0100

    changed and added

--- terraform/create_config_ec2.tf
+++ terraform/create_config_ec2.tf
@@ -1,3 +1,5 @@
+
+
 terraform {
   required_providers {
     aws = {
@@ -32,45 +34,31 @@ locals {
   ssh_key_name = "myawsKey"
 }
 
-resource "aws_vpc" "sulaiman_vpc" {
-  cidr_block       = "10.0.0.0/16"
-
-  tags = {
-    Name = "sulaiman_vpc"
-  }
+resource "aws_default_vpc" "default" {
+    tags = {
+        Name = "default"
+    }
 }
 
-resource "aws_subnet" "sulaiman_public_subnet" {
-  vpc_id      = aws_vpc.sulaiman_vpc.id
-  cidr_block  = "10.0.1.0/24"
-  availability_zone = "eu-west-3a"
 
-  tags = {
-    Name = "sulaiman_pub_subnet"
-  }
-}
-
-resource "aws_subnet" "sulaiman_private_subnet" {
-  vpc_id      = aws_vpc.sulaiman_vpc.id
-  cidr_block  = "10.0.2.0/24"
-  availability_zone = "eu-west-3a"
-
-  tags = {
-    Name = "sulaiman_private_subnet"
-  }
-}
-
-resource "aws_security_group" "sulaiman_web_sg" {
-  name        = "sulaiman_ssh_http"
-  vpc_id      = aws_vpc.sulaiman_vpc.id
+resource "aws_security_group" "devops_sg" {
+  name        = "devops_ssh_http"
+  vpc_id      = aws_default_vpc.default.id
   description = "Allow SSH access"
+
+  /* ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  } */
 
   ingress {
         from_port = 22
@@ -89,67 +77,37 @@ resource "aws_security_group" "sulaiman_web_sg" {
 
 
   tags = {
-    Name = "sulaiman_web_sg"
+    Name = "devops_sg"
   }
 }
 
-resource "aws_security_group" "sulaiman_ssh_sg" {
-  name        = "sulaiman_ssh"
-  vpc_id      = aws_vpc.sulaiman_vpc.id
-  description = "Allow SSH access"
+resource "aws_instance" "managed_nodes" {
+  ami                             = "ami-04b7bf9494d21c5bb"
+  instance_type                   = "t2.micro"
+  key_name                        = aws_key_pair.myawskey.key_name
 
- ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-        description = "SSH from any ip"
-    }
+  vpc_security_group_ids          = [aws_security_group.devops_sg.id]
+  associate_public_ip_address     = true
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkdir /opt/app",
+    ]
+  }
 
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = tls_private_key.mypem.private_key_pem
+    host        = self.public_ip
+  }
 
+  count                           = 2
   tags = {
-    Name = "sulaiman_ssh_sg"
-  }
-}
-
-# Setup Internet gateway
-resource "aws_internet_gateway" "sulaiman_igw" {
-  vpc_id = aws_vpc.sulaiman_vpc.id
-
-  tags = {
-    Name = "sulaiman_igw"
-  }
-}
-
-# Setup Internet router
-resource "aws_route_table" "sulaiman_public_rt" {
-  vpc_id = aws_vpc.sulaiman_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.sulaiman_igw.id
+    Name = "devops-managed-node-${count.index + 1}"
+    Type = "managed-nodes"
   }
 
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.sulaiman_igw.id
-  }
-
-  tags = {
-    Name = "sulaiman_public_rt"
-  }
-}
-
-resource "aws_route_table_association" "sulaiman_public_rta" {
-  subnet_id      = aws_subnet.sulaiman_public_subnet.id
-  route_table_id = aws_route_table.sulaiman_public_rt.id
 }
 
 resource "aws_instance" "control_node" {
@@ -157,37 +115,37 @@ resource "aws_instance" "control_node" {
   instance_type                   = "t2.micro"
   key_name                        = aws_key_pair.myawskey.key_name
 
-  subnet_id                       = aws_subnet.sulaiman_public_subnet.id
-  vpc_security_group_ids          = [aws_security_group.sulaiman_web_sg.id]
+  vpc_security_group_ids          = [aws_security_group.devops_sg.id]
   associate_public_ip_address     = true
-
-  user_data = <<-EOF
-  #!/bin/bash -ex
-
-  amazon-linux-extras install nginx1 -y
-  echo "<h1>The simplest thing is to learn. Congratulations, you succeeded!</h1>" >  /usr/share/nginx/html/index.html 
-  systemctl enable nginx
-  systemctl start nginx
-  EOF
   
   tags = {
-    Name = "sulaiman-control-node"
+    Name = "devops-control-node"
     Type = "control-node"
   }
-}
 
-resource "aws_instance" "managed_node" {
-  ami                             = "ami-0302f42a44bf53a45"
-  instance_type                   = "t2.micro"
-  key_name                        = aws_key_pair.myawskey.key_name
-
-  subnet_id                       = aws_subnet.sulaiman_private_subnet.id
-  vpc_security_group_ids          = [aws_security_group.sulaiman_ssh_sg.id]
-  associate_public_ip_address     = false
-  count = 2
-
-  tags = {
-    Name = "sulaiman-managed-node-${count.index + 1}"
-    Type = "managed-node"
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras install -y epel",
+      "sudo useradd devops",
+      "echo -e 'devops\ndevops' | sudo passwd devops",
+      "echo 'devops ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/devops",
+      "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config",
+      "sudo systemctl restart sshd.service",
+      "sudo yum update -y",
+      "sudo yum install -y ansible",
+      "sudo mkdir /etc/ansible",
+      "sudo touch /etc/ansible/ansible.cfg",
+      "sudo echo '[defaults]\ninventory = /etc/ansible/host\nhost_key_checking = False\nremote_user = devops' > /etc/ansible/ansible.cfg",
+      "sudo ansible-config init --disabled > ansible.cfg",
+      "sudo touch /etc/ansible/hosts",
+      "sudo echo '[control-node]\ndevops-control-node ansible_host=10.0.1.149 ansible_user=ec2_user\n[managed-nodes]\ndevops-managed-node-1 ansible_host=10.0.2.186 ansible_user=ec2_user\ndevops-managed-node-2 ansible_host=10.0.1.24 ansible_user=ec2_user' > /etc/ansible/hosts"
+    ]
   }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = tls_private_key.mypem.private_key_pem
+    host        = self.public_ip
+  } 
 }
